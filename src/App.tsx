485f2135178888b82f6e03679c55bf5dcfe506ba
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-07-14 19:10:27 +0700

    chore: Remove commented out code and unused imports in App and NewTodo components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import NewTodo from "./components/NewTodo";
 import Sidebar from "./components/Sidebar";
-import Todos from "./components/Todos";
-import Todo from "./models/todo";
+// import Todos from "./components/Todos";
+// import Todo from "./models/todo";
 
 function App() {
-  const todos: Todo[] = [
-    {
-      id: +new Date() + Math.random(),
-      title: "learn react with js",
-      description: "i want to learn react with js",
-    },
-    {
-      id: +new Date() + Math.random(),
-      title: "learn react with ts",
-      description: "i want to learn react with ts",
-    },
-  ];
+  // const todos: Todo[] = [
+  //   {
+  //     id: +new Date() + Math.random(),
+  //     title: "learn react with js",
+  //     description: "i want to learn react with js",
+  //   },
+  //   {
+  //     id: +new Date() + Math.random(),
+  //     title: "learn react with ts",
+  //     description: "i want to learn react with ts",
+  //   },
+  // ];
   return (
     <div className="flex">
       <Sidebar />
-      <div className="p-10 w-full h-full">
-        <Todos items={todos} />
+      <div className="w-full h-full">
+        {/* <Todos items={todos} /> */}
         <NewTodo />
       </div>
     </div>
--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -9,7 +9,7 @@ const NewTodo = () => {
   const {
     register,
     handleSubmit,
-    watch,
+    //watch,
     formState: { errors },
   } = useForm<NewTodoInput>({ mode: "onBlur" });
 
@@ -17,56 +17,69 @@ const NewTodo = () => {
     console.log(data);
   };
 
-  const isTitleEmpty = watch("title");
-  const isDesctiptionEmpty = watch("description");
+  // const isTitleEmpty = watch("title");
+  // const isDesctiptionEmpty = watch("description");
 
-  const isFormValid = isTitleEmpty && isDesctiptionEmpty;
+  // const isFormValid = isTitleEmpty && isDesctiptionEmpty;
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="w-full">
-      <div className="flex flex-col my-2">
-        <label htmlFor="title" className="font-semibold">
-          Title
-        </label>
-        <input
-          {...register("title", { required: true })}
-          type="text"
-          name="title"
-          id="title"
-          placeholder="Masukkan Judul"
-          className={`w-1/3 border ${
-            errors.title
-              ? "border-red-500 focus:outline-none focus:ring-1 focus:ring-red-500"
-              : "border-gray-300"
-          } rounded p-1`}
-        />
-        {errors.title && <p className="text-red-500">This field is required</p>}
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col p-4 m-5 rounded border border-gray-300 shadow-md"
+    >
+      <div className="flex justify-between mb-2 border-b-2 pb-2">
+        <h2 className="text-4xl font-bold">Add New Todo</h2>
+        <button className="px-3 py-1 bg-red-500 text-white rounded hover:bg-red-600">
+          Back
+        </button>
       </div>
-      <div className="flex flex-col my-2">
-        <label htmlFor="description" className="font-semibold">
-          Description
-        </label>
-        <textarea
-          {...register("description", { required: true })}
-          name="description"
-          id="description"
-          placeholder="Masukkan Deskripsi"
-          className={`w-1/2 h-32 border ${
-            errors.description
-              ? "border-red-500 focus:outline-none focus:ring-1 focus:ring-red-500"
-              : "border-gray-300"
-          } rounded p-1`}
-        />
-        {errors.description && (
-          <p className="text-red-500">This field is required</p>
-        )}
+      <div>
+        <div className="flex flex-col my-2">
+          <label htmlFor="title" className="font-semibold">
+            Title
+          </label>
+          <input
+            {...register("title", { required: true })}
+            type="text"
+            name="title"
+            id="title"
+            placeholder="Masukkan Judul"
+            className={`w-1/3 border ${
+              errors.title
+                ? "border-red-500 focus:outline-none focus:ring-1 focus:ring-red-500"
+                : "border-gray-300"
+            } rounded p-1`}
+          />
+          {errors.title && (
+            <p className="text-red-500">This field is required</p>
+          )}
+        </div>
+        <div className="flex flex-col my-2">
+          <label htmlFor="description" className="font-semibold">
+            Description
+          </label>
+          <textarea
+            {...register("description", { required: true })}
+            name="description"
+            id="description"
+            placeholder="Masukkan Deskripsi"
+            className={`w-1/2 h-32 border ${
+              errors.description
+                ? "border-red-500 focus:outline-none focus:ring-1 focus:ring-red-500"
+                : "border-gray-300"
+            } rounded p-1`}
+          />
+          {errors.description && (
+            <p className="text-red-500">This field is required</p>
+          )}
+        </div>
+        <button
+          //disabled={!isFormValid}
+          className="px-4 py-2 disabled:bg-gray-500 bg-gray-700 text-white rounded hover:bg-gray-800"
+        >
+          Add Todo
+        </button>
       </div>
-      <button
-        disabled={!isFormValid}
-        className="px-4 py-2 disabled:bg-gray-500 bg-gray-700 text-white rounded hover:bg-gray-800"
-      >
-        Add Todo
-      </button>
     </form>
   );
 };
