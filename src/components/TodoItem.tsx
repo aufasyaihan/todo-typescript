f127aec64b3f1f327dd1777381ffeaa28ae3c06b
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-08-03 16:50:21 +0700

    fix: fixed logic of dates

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -11,8 +11,8 @@ const TodoItem: React.FC = () => {
   const todo = todos.find((todo) => todo.id === +params.id!);
   const navigate = useNavigate();
 
-  const currentDate = new Date().toLocaleDateString();
-  const todoDate = todo!.date.toLocaleDateString();
+  const currentDate = new Date().toISOString().split("T")[0];
+  const todoDate = todo!.date.toISOString().split("T")[0];
 
   const dispatch = useDispatch();
 
@@ -35,9 +35,14 @@ const TodoItem: React.FC = () => {
   return (
     <section className="flex flex-col gap-5 p-4 md:m-5 rounded border border-gray-300 shadow-md">
       <div className="flex gap-6 items-start flex-col-reverse md:flex-row border-b border-gray-400 justify-between md:items-center pb-2">
-        <h3 className="text-2xl w-full md:text-3xl font-bold ">{todo?.title}</h3>
+        <h3 className="text-2xl w-full md:text-3xl font-bold ">
+          {todo?.title}
+        </h3>
         <div className="flex gap-2">
-          <Link to="/" className="px-4 py-2 rounded bg-gray-700 text-white hover:bg-gray-800">
+          <Link
+            to="/"
+            className="px-4 py-2 rounded bg-gray-700 text-white hover:bg-gray-800"
+          >
             Home
           </Link>
           <Button
